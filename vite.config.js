e7c03a5164a3652519d0b29e587ce23c370c1058
Ashish Singh Chauhan <117079261+Ashish-NIT-Goa@users.noreply.github.com>
2025-04-07 17:02:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Ashish-NIT-Goa.github.io/"
+  base: "/Ashish-NIT-Goa.github.io/",
 })
